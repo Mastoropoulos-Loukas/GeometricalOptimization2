1282f2e3024831b2ea59f50115b3f7e52274538c
aqqes <runeghost@gmail.com>
2022-12-07 11:14:03 +0200

    fixed tabs

--- ant.cpp
+++ ant.cpp
@@ -335,283 +335,266 @@ double MaxOrMin(int area,int mode)
   
 Polygon_2 Ant::optimalPolygon(){
 
-std::vector<Polygon_2> space;
-std::vector<Polygon_2> temp;
-std::vector<Polygon_2> adder;
-Polygon_2 next;
-std::vector<Point> test;
-Polygon_2 check;
-test=this->list;
-int max=-1;
-int min=INT_MAX;
-int min1=INT_MAX;
-int max1=-1;
+    std::vector<Polygon_2> space;
+    std::vector<Polygon_2> temp;
+    std::vector<Polygon_2> adder;
+    Polygon_2 next;
+    std::vector<Point> test;
+    Polygon_2 check;
+    test=this->list;
+    int max=-1;
+    int min=INT_MAX;
+    int min1=INT_MAX;
+    int max1=-1;
 
-int pos=0;
-double r=argFlags.ro;
-std::map<std::string, int> enumvals;
-std::map<int,std::vector<Polygon_2>> polymap;
-std::vector <std::vector<Polygon_2>> map;
-std::vector<std::list<table>> table1;
-table table2;
-std::vector <int> num;
-std::map <int,std::vector<ant>> tables;
-std::srand(time(0));
-Polygon_2 BestFor1Ant;
-int mode=argFlags.optimizationType; //0 for max 1 for min 
-int dupes=0;
-  int elitismpos=0;
+    int pos=0;
+    double r=argFlags.ro;
+    std::map<std::string, int> enumvals;
+    std::map<int,std::vector<Polygon_2>> polymap;
+    std::vector <std::vector<Polygon_2>> map;
+    std::vector<std::list<table>> table1;
+    table table2;
+    std::vector <int> num;
+    std::map <int,std::vector<ant>> tables;
+    std::srand(time(0));
+    Polygon_2 BestFor1Ant;
+    int mode=argFlags.optimizationType; //0 for max 1 for min 
+    int dupes=0;
+    int elitismpos=0;
     int elitismk=0;
-Polygon_2 BestForCircle;
- space= Generate3(test);
- int AreaOfAllTriangles=0;
- for(auto v=space.begin();v!=space.end();++v){
- enumvals[convert(v[0])]=pos;
-  table2.h=v[0].area();
-AreaOfAllTriangles+=abs(v[0].area());
-  table2.poly=pos;
-  table1.resize(table1.size()+1);
-  table1=add_edge(table1,pos,table2,nullptr);
-  
-  pos++;
+    Polygon_2 BestForCircle;
+    space= Generate3(test);
+    int AreaOfAllTriangles=0;
+    for(auto v=space.begin();v!=space.end();++v){
+        enumvals[convert(v[0])]=pos;
+        table2.h=v[0].area();
+        AreaOfAllTriangles+=abs(v[0].area());
+        table2.poly=pos;
+        table1.resize(table1.size()+1);
+        table1=add_edge(table1,pos,table2,nullptr);
+        pos++;
 
- }
-AreaOfAllTriangles=AreaOfAllTriangles/space.size();
-
-map.push_back(space);
-std::list<table> tablebot;
-  int K=list.size()/4;
-   int C=argFlags.L;
-   std::vector <int> paths[K];
-
-   ph=2;
-   int elitism=argFlags.elitism;
-   std::cout<<pos<<std::endl;
-for(int c=0;c<C;c++){
-for(int k=0;k<K;k++){
-  temp.clear();
-  num.clear();
-  adder.clear();
-int triangle=0;
-double prob=0;
-
-
-for (auto t1=space.begin();t1!=space.end();++t1,++triangle){
-
-if(mode==1)
-{
-
-prob=AreaOfAllTriangles/t1[0].area();
-prob/=100;
- int var=rand()%2;
-
- if(var<prob)
-{
-  next=t1[0];
-
-
-break;
-}
-}
-else{
-prob=t1[0].area()/AreaOfAllTriangles;
-prob/=100;
- int var=rand()%2;
-
- if(var<prob)
-{
-  next=t1[0];
-
-
-break;
-}
-}
- if(triangle==space.size()-1)
-{
-  next=t1[0];
-
-break;
-}
-}
-tablebot.clear();
-
-std::vector<Point> test1=test;
-
-for(int i=2;i<=test.size();i++){
-
-
- num.push_back(enumvals[convert(next)]);
- if(i==test.size()-1)
- break;
-
-if(polymap.find(enumvals[convert(next)])!=polymap.end()){
-
-temp=polymap[enumvals[convert(next)]];
-
-}else{
-
- temp= GenerateX(next,test1);
- polymap[enumvals[convert(next)]]=temp;
-
-}
-
-
- for (auto t1=temp.begin();t1!=temp.end();++t1){
- 
-  if(map.size()<=i-2){
-  
-  adder.push_back(t1[0]);
-  enumvals[convert(t1[0])]=pos;
-  pos++;
-  table2.poly=enumvals[convert(t1[0])];
-  if(mode==0)
-  table2.h=t1[0].area()-next.area() ;
-  else
-    table2.h=1/(t1[0].area()-next.area()) ;
-
-  table1.resize(table1.size()+1);
-  table1=add_edge(table1,enumvals[convert(next)],table2,nullptr);
-  tablebot.push_back(table2);
-  }
-  else
- if(!isin(map[i-2],t1[0])){
-  enumvals[convert(t1[0])]=pos;
-  pos++;
-  table2.poly=enumvals[convert(t1[0])];
-   if(mode==0)
-  table2.h=t1[0].area()-next.area() ;
-  else
-    table2.h=1/(t1[0].area()-next.area()) ;
-    table1.resize(table1.size()+1);
-
-  table1=add_edge(table1,enumvals[convert(next)],table2,nullptr);
-
-  tablebot.push_back(table2);
-  adder.push_back(t1[0]);
-
- }else{
-dupes++;
-  table2.poly=enumvals[convert(t1[0])];
-    table1.resize(table1.size()+1);
-    table2.hasbranch=1;
-          ant antt;
-        antt.father=enumvals[convert(next)];
-         if(mode==0)
-  table2.h=t1[0].area()-next.area() ;
-  else
-    table2.h=1/(t1[0].area()-next.area()) ;
-
-  table1=add_edge(table1,enumvals[convert(next)],table2,&antt.pos);
-    tables[enumvals[convert(t1[0])]].push_back(antt);
-  tablebot.push_back(table2);
- }
- table2.hasbranch=0;
-  
- }
- 
- map.push_back(adder);
-
- adder.clear();
-
-int t=0;
- for (auto t1=temp.begin();t1!=temp.end();++t1,++t){
-std::list<table>::iterator it=tablebot.begin();
-advance(it,t);
-double prob=ProbFunction(argFlags.alpha,argFlags.beta,*it,tablebot);
- int var=rand()%2;
- if(var<prob)
-{
-  next=t1[0];
-
-
-break;
-}
- if(t==temp.size()-1)
-{
-  next=t1[0];
-
-break;
-}
-}
-if(temp.size()!=0)
-test1.erase(test1.begin()+Pointpoped[convert(next)]);
-}
- 
-
-
-
-  
-  paths[k]=num;
-num.clear();
-
-  for(auto it1 = table1.begin(); it1 != table1.end(); ++it1) {
-
-        
-        std::list<table>::iterator it;
-for (it = it1[0].begin(); it != it1[0].end(); ++it){
-     it->t*=(1-r);
-    
     }
+    AreaOfAllTriangles=AreaOfAllTriangles/space.size();
+
+    map.push_back(space);
+    std::list<table> tablebot;
+    int K=list.size()/4;
+    int C=argFlags.L;
+    std::vector <int> paths[K];
+
+    ph=2;
+    int elitism=argFlags.elitism;
+    std::cout<<pos<<std::endl;
+    for(int c=0;c<C;c++){
+        for(int k=0;k<K;k++){
+            temp.clear();
+            num.clear();
+            adder.clear();
+            int triangle=0;
+            double prob=0;
+
+
+            for (auto t1=space.begin();t1!=space.end();++t1,++triangle){
+
+                if(mode==1)
+                {
+
+                    prob=AreaOfAllTriangles/t1[0].area();
+                    prob/=100;
+                    int var=rand()%2;
+
+                    if(var<prob)
+                    {
+                        next=t1[0];
+                        break;
+                    }
+                }
+                else{
+                    prob=t1[0].area()/AreaOfAllTriangles;
+                    prob/=100;
+                    int var=rand()%2;
+                    if(var<prob)
+                    {
+                        next=t1[0];
+                        break;
+                    }
+                }
+                if(triangle==space.size()-1)
+                {
+                    next=t1[0];
+                    break;
+                }
+            }
+            tablebot.clear();
+
+            std::vector<Point> test1=test;
+
+            for(int i=2;i<=test.size();i++){
+
+
+                num.push_back(enumvals[convert(next)]);
+                if(i==test.size()-1)
+                    break;
+
+                if(polymap.find(enumvals[convert(next)])!=polymap.end()){
+
+                    temp=polymap[enumvals[convert(next)]];
+
+                }else{
+
+                    temp= GenerateX(next,test1);
+                    polymap[enumvals[convert(next)]]=temp;
+
+                }
+
+
+                for (auto t1=temp.begin();t1!=temp.end();++t1){
+    
+                    if(map.size()<=i-2){
+    
+                        adder.push_back(t1[0]);
+                        enumvals[convert(t1[0])]=pos;
+                        pos++;
+                        table2.poly=enumvals[convert(t1[0])];
+                        if(mode==0)
+                            table2.h=t1[0].area()-next.area() ;
+                        else
+                            table2.h=1/(t1[0].area()-next.area()) ;
+
+                        table1.resize(table1.size()+1);
+                        table1=add_edge(table1,enumvals[convert(next)],table2,nullptr);
+                        tablebot.push_back(table2);
+                    }
+                    else
+                    if(!isin(map[i-2],t1[0])){
+                        enumvals[convert(t1[0])]=pos;
+                        pos++;
+                        table2.poly=enumvals[convert(t1[0])];
+                        if(mode==0)
+                            table2.h=t1[0].area()-next.area() ;
+                        else
+                            table2.h=1/(t1[0].area()-next.area()) ;
+                        
+                        table1.resize(table1.size()+1);
+
+                        table1=add_edge(table1,enumvals[convert(next)],table2,nullptr);
+
+                        tablebot.push_back(table2);
+                        adder.push_back(t1[0]);
+
+                    }else{
+                        dupes++;
+                        table2.poly=enumvals[convert(t1[0])];
+                        table1.resize(table1.size()+1);
+                        table2.hasbranch=1;
+                        ant antt;
+                        antt.father=enumvals[convert(next)];
+                        if(mode==0)
+                            table2.h=t1[0].area()-next.area() ;
+                        else
+                            table2.h=1/(t1[0].area()-next.area()) ;
+
+                        table1=add_edge(table1,enumvals[convert(next)],table2,&antt.pos);
+                        tables[enumvals[convert(t1[0])]].push_back(antt);
+                        tablebot.push_back(table2);
+                    }
+                    table2.hasbranch=0;
+    
+                }
+    
+                map.push_back(adder);
+
+                adder.clear();
+
+                int t=0;
+                for (auto t1=temp.begin();t1!=temp.end();++t1,++t){
+                    std::list<table>::iterator it=tablebot.begin();
+                    advance(it,t);
+                    double prob=ProbFunction(argFlags.alpha,argFlags.beta,*it,tablebot);
+                    int var=rand()%2;
+                    if(var<prob)
+                    {
+                        next=t1[0];
+                        break;
+                    }
+                    if(t==temp.size()-1)
+                    {
+                        next=t1[0];
+                        break;
+                    }
+                }
+                if(temp.size()!=0)
+                    test1.erase(test1.begin()+Pointpoped[convert(next)]);
+            }
+    
+            paths[k]=num;
+            num.clear();
+
+            for(auto it1 = table1.begin(); it1 != table1.end(); ++it1) {
+                std::list<table>::iterator it;
+                for (it = it1[0].begin(); it != it1[0].end(); ++it){
+                    it->t*=(1-r);
+                }
+
+            }
+            if(mode==0){
+                if(max<abs(next.area())){
+
+                    max=abs(next.area());
+                    BestFor1Ant=next;
+                    elitismk=k;
+                }
+            }else{
+    
+                if(min>abs(next.area())){
+
+                    min=abs(next.area());
+                    BestFor1Ant=next;
+                    elitismk=k;
+                }
+            }
+        }
+        if(mode==0){
+            if(max1<abs(BestFor1Ant.area())){
+
+                max1=abs(BestFor1Ant.area());
+                BestForCircle=BestFor1Ant;
+                elitismpos=elitismk;
+            }
+        }
+        else{
+            if(min1>abs(BestFor1Ant.area())){
+
+                min1=abs(BestFor1Ant.area());
+                BestForCircle=BestFor1Ant;
+                elitismpos=elitismk;
+            }
+        }
+
+        if(elitism==0){
+            for( int t=0;t<K;t++)
+                table1=UpdateTrails(table1,paths[t],space[0].area(),tables,MaxOrMin(space[0].area(),mode));
+        }
+        else{
+            table1=UpdateTrails(table1,paths[elitismpos],space[0].area(),tables,MaxOrMin(space[0].area(),mode));
+
 
         }
-if(mode==0){
-if(max<abs(next.area())){
+    }
 
- max=abs(next.area());
- BestFor1Ant=next;
-  elitismk=k;
-}
-}else{
-  
-if(min>abs(next.area())){
-
- min=abs(next.area());
- BestFor1Ant=next;
-  elitismk=k;
-}
-}
-}
-if(mode==0){
-if(max1<abs(BestFor1Ant.area())){
-
- max1=abs(BestFor1Ant.area());
- BestForCircle=BestFor1Ant;
- elitismpos=elitismk;
-}
-}
-else{
-if(min1>abs(BestFor1Ant.area())){
-
- min1=abs(BestFor1Ant.area());
- BestForCircle=BestFor1Ant;
- elitismpos=elitismk;
-}
-}
-
-if(elitism==0){
-  for( int t=0;t<K;t++)
-  table1=UpdateTrails(table1,paths[t],space[0].area(),tables,MaxOrMin(space[0].area(),mode));
-}
-  else{
-  table1=UpdateTrails(table1,paths[elitismpos],space[0].area(),tables,MaxOrMin(space[0].area(),mode));
+    Polygon_2 poly;
 
 
-  }
-}
+    poly=BestForCircle;
+    std::ofstream os("test.wkt");
 
-  Polygon_2 poly;
-
-
-  poly=BestForCircle;
-  std::cout<<poly.area() <<std::endl;
-      std::ofstream os("test.wkt");
-
-  return poly;
+    return poly;
 
 
 
 
-}
+    }
 
 
 //find the poss edges that can be replaced
